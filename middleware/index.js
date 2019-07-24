b9a5ccbf3db0b5383739e603b3bdf360acd15976
Santis Aggelos <santis.aggelos@gmail.com>
2019-07-24 09:39:27 +0300

    Fix minor typos in middleware, fix async package vulnerability

--- middleware/index.js
+++ middleware/index.js
@@ -6,19 +6,19 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next) {
     if(req.isAuthenticated()) {
         Campground.findOne({slug: req.params.slug}, function(err, foundCampground) {
             if(err) {
-                req.flash('error', 'Campground not found');
+                req.flash('error', 'Campground not found.');
                 res.redirect('/campgrounds');
             } else {
                 if(foundCampground.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next();   
                 } else {
-                    req.flash('error', 'You do not have permission to do that');
+                    req.flash('error', 'You do not have permission to do that.');
                     res.redirect('back');
                 }
             }
         });
     } else {
-        req.flash('error', 'You must be signed in to do that');
+        req.flash('error', 'You must be signed in to do that.');
         res.redirect('back');
     }
 }
@@ -32,13 +32,13 @@ middlewareObj.checkCommentOwnership = function(req, res, next) {
                 if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin) {
                     next();   
                 } else {
-                    req.flash('error', 'You do not have permission to do that');
+                    req.flash('error', 'You do not have permission to do that.');
                     res.redirect('back');
                 }
             }
         });
     } else {
-        req.flash('error', 'You must be signed in to do that');
+        req.flash('error', 'You must be signed in to do that.');
         res.redirect('back');
     }
 }
@@ -47,7 +47,7 @@ middlewareObj.isLoggedIn = function(req, res, next) {
     if(req.isAuthenticated()) {
         return next();
     }
-    req.flash('error', 'You must be signed in to do that');
+    req.flash('error', 'You must be signed in to do that.');
     res.redirect('/login');
 }
 
