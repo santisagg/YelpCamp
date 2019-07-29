21ddd86b3a1d195fac0c57be84f9bc5bdf5a5495
Santis Aggelos <santis.aggelos@gmail.com>
2019-07-29 11:45:03 +0300

    Fix small styling issues

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose'),
 const userSchema = new mongoose.Schema({
     username: {type: String, unique: true, required: true},
     password: String,
-    avatar: String,
+    avatar: {type: String, default: ''},
     firstName: String,
     lastName: String,
     email: {type: String, unique: true, required: true},
